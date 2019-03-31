cfadac7516fed53abbf6a22f2a2e83b12d1c88de
000Justin000 <juntengjia@hotmail.com>
2019-03-30 22:10:56 -0400

    update script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-taskset --cpu-list 0 python tnode.py --dataset exponential_hawkes --niters 2000 --jump_type read --batch_size 30 --nsave 20 --evnt_align --restart > exponential_hawkes/7612c40.log 2>exponential_hawkes/7612c40.error &
-taskset --cpu-list 1 python tnode.py --dataset self_inhibiting --niters 2000 --jump_type read --batch_size 30 --nsave 20 --evnt_align --restart > self_inhibiting/7612c40.log 2>self_inhibiting/7612c40.error &
-taskset --cpu-list 2 python tnode.py --dataset powerlaw_hawkes --niters 2000 --jump_type read --batch_size 30 --nsave 20 --evnt_align --restart > powerlaw_hawkes/7612c40.log 2>powerlaw_hawkes/7612c40.error &
+taskset --cpu-list 0 python tnode.py --dataset exponential_hawkes --niters 2000 --jump_type read --batch_size 30 --nsave 20 --evnt_align --restart > exponential_hawkes/$1.log 2>exponential_hawkes/$1.error &
+taskset --cpu-list 1 python tnode.py --dataset self_inhibiting --niters 2000 --jump_type read --batch_size 30 --nsave 20 --evnt_align --restart > self_inhibiting/$1.log 2>self_inhibiting/$1.error &
+taskset --cpu-list 2 python tnode.py --dataset powerlaw_hawkes --niters 2000 --jump_type read --batch_size 30 --nsave 20 --evnt_align --restart > powerlaw_hawkes/$1.log 2>powerlaw_hawkes/$1.error &
