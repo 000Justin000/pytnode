62bea0a8201cbe861aabd9c778d519a9b9db2650
000Justin000 <juntengjia@hotmail.com>
2019-04-17 15:23:46 -0400

    added params.pth at step 5000 for three body

--- combine.py
+++ combine.py
@@ -12,4 +12,4 @@ for filename in os.listdir(path):
     if filename[-4:] == ".png":
         itr, nid, tid = filename[:-4].split('_')
         if itr == suffix:
-            os.system("convert -append" + "    " + os.path.join(path, "00000_"+nid+"_"+tid+".png") + "    " + os.path.join(path, filename) + "    " + os.path.join(path, "combine/"+nid+"_"+tid+".png"))
+            os.system("convert -append" + "    " + os.path.join(path, "000000_"+nid+"_"+tid+".png") + "    " + os.path.join(path, filename) + "    " + os.path.join(path, "combine/"+nid+"_"+tid+".png"))
